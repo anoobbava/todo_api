6dd9c4734ade63662dc69d5f5be1a5070de29be8
Anoob Bava <anoob.bava@gmail.com>
2019-08-03 11:22:24 +0530

    added controller actions for todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :todos
 end
+
+# get /todos/new
+# post /todos
+# get /todos
+# get /todos/:id >> show
+# get /todos/:id/edit >> edit
+# put/patch /todos/:id >> update
+# delete /todos/:id >> delete
